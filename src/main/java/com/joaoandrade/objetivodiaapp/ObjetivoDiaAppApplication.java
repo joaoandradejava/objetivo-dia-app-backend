0d443218dbfdeac88de086a01556da12aba3273f
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-08-11 17:08:20 -0300

    Primeiro commit

---
+++ src/main/java/com/joaoandrade/objetivodiaapp/ObjetivoDiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.objetivodiaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivoDiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivoDiaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoandrade/objetivodiaapp/ObjetivoDiaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.objetivodiaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivoDiaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
